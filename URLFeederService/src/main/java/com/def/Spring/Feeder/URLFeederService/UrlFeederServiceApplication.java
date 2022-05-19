68790902e2f2523c2c10c6939cd71d026403f51d
Tunir <vincenthavoc@gmail.com>
2022-05-19 17:22:52 +0530

    commit - initial commit

---
+++ URLFeederService/src/main/java/com/def/Spring/Feeder/URLFeederService/UrlFeederServiceApplication.java
@@ -0,0 +1,13 @@
+package com.def.Spring.Feeder.URLFeederService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrlFeederServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrlFeederServiceApplication.class, args);
+	}
+
+}
---
+++ URLFeederService/src/main/java/com/def/Spring/Feeder/URLFeederService/resources/URLResources.java
@@ -0,0 +1,24 @@
+package com.def.Spring.Feeder.URLFeederService.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class URLResources {
+
+	@GetMapping("/ping")
+	public String ping() {
+		return "pong";
+	}
+	
+	@PostMapping
+	public ResponseEntity<Void> submitURL(@RequestBody String url) {
+		System.out.println(url);
+		return ResponseEntity.ok().build();
+	}
+	
+}
+
